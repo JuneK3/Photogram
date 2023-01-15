f8af4cc8f5fa24935ce856c66477b7bbb365ec60
Junek3 <rlarudrms28@gmail.com>
2023-01-15 16:30:56 +0900

    feat: csrf 공격방지 기능을 위해 view 파일에 csrf 토큰 추가하기
    csrf 토큰 없이 요청을 보내면 403 Forbidden 에러 발생

---
+++ src/main/java/com/rootlab/photogram/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.rootlab.photogram.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+//		super.configure(http); // 기존 security가 제공하는 기능들 비활성화
+//		http.csrf().disable(); // csrf 기능을 끄지 않고 유지
+		http.authorizeRequests()
+				.antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**")
+				.authenticated()
+				.anyRequest().permitAll()
+				.and()
+				.formLogin()
+				.loginPage("/auth/signin")
+				.defaultSuccessUrl("/");
+
+	}
+}

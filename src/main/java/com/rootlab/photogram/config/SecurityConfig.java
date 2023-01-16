96a2bc4e845b4c99a72f8eb1e988bda0de90c732
Junek3 <rlarudrms28@gmail.com>
2023-01-16 13:23:43 +0900

    feat: 회원가입시 user 권한 설정 및 password 암호화하기
    User Entity의 username에 unique 제약조건 설정하기
    AuthService의 saveUser에서 password를 암호화하고 User 권한 설정후 save하도록 구현

--- src/main/java/com/rootlab/photogram/config/SecurityConfig.java
+++ src/main/java/com/rootlab/photogram/config/SecurityConfig.java
@@ -1,11 +1,20 @@
 package com.rootlab.photogram.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //		super.configure(http); // 기존 security가 제공하는 기능들 비활성화

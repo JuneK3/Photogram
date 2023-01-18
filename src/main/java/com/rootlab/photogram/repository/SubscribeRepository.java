cb0d7d6dab792860755b461f1690203558eb2a12
Junek3 <rlarudrms28@gmail.com>
2023-01-19 06:18:45 +0900

    [#5] feat: Subscribe 엔티티 생성하기
    SubscribeRepository 생성
    column name을 snake case로 변경하지 않고 변수명 그대로 사용하도록 naming strategy를 변경

---
+++ src/main/java/com/rootlab/photogram/repository/SubscribeRepository.java
@@ -0,0 +1,7 @@
+package com.rootlab.photogram.repository;
+
+import com.rootlab.photogram.domain.Subscribe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
+}

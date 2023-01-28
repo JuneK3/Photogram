0112ad6cc1cc2990d79abfa92f61d605f8f7e3a8
Junek3 <rlarudrms28@gmail.com>
2023-01-28 10:27:35 +0900

    [#8] feat: Likes 엔티티 작성하기
    좋아요 버튼이 작동하도록 js코드 수정

---
+++ src/main/java/com/rootlab/photogram/domain/Likes.java
@@ -0,0 +1,45 @@
+package com.rootlab.photogram.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                name = "likes_unique_constraint",
+                columnNames = {"userId", "imageId"}
+        )
+})
+public class Likes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JoinColumn(name = "imageId")
+    @ManyToOne
+    private Image image;
+
+    @JsonIgnoreProperties({"images"})
+    @JoinColumn(name = "userId")
+    @ManyToOne
+    private User user;
+
+    private LocalDateTime createDate;
+
+    @PrePersist
+    public void createDate() {
+        this.createDate = LocalDateTime.now();
+    }
+
+}
